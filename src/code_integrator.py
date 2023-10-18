8ba60c51441010150e5472b9802f78cd6167002b
I032059 <faye.he@sap.com>
2023-10-18 16:03:35 +0800

    files

--- src/code_integrator.py
+++ src/code_integrator.py
@@ -1,23 +1,40 @@
-class CodeIntegrator:  
-    def __init__(self, filepath):  
-        self.filepath = filepath  
+import os  
   
-    def insert_after(self, target_line, code):  
-        with open(self.filepath, 'r') as file:  
+class CodeInserter:  
+    def __init__(self, input_filepath, output_filepath):  
+        self.input_filepath = input_filepath  
+        self.output_filepath = output_filepath  
+  
+    def insert_after(self, target_string, code):  
+        with open(self.input_filepath, 'r') as file:  
             lines = file.readlines()  
   
-        # Find the line number of the target line  
-        line_number = next((i for i, line in enumerate(lines) if target_line in line), None)  
+        # Find the line number of the first line containing the target string  
+        line_number = next((i for i, line in enumerate(lines) if target_string in line), None)  
   
         if line_number is not None:  
             # Insert the code after the target line  
             lines.insert(line_number + 1, code + '\n')  
   
-            with open(self.filepath, 'w') as file:  
+            # Check if the output directory exists, if not, create it  
+            output_dir = os.path.dirname(self.output_filepath)  
+            if not os.path.exists(output_dir):  
+                os.makedirs(output_dir)  
+  
+            # Check if the output file exists, if so, remove it  
+            if os.path.exists(self.output_filepath):  
+                os.remove(self.output_filepath)  
+  
+            with open(self.output_filepath, 'w') as file:  
                 file.writelines(lines)  
         else:  
-            print(f"Target line '{target_line}' not found in the file.")  
+            print(f"No line containing '{target_string}' was found in the file.")  
   
+
 # Usage  
-inserter = CodeInserter('your_file_path_here')  
-inserter.insert_after('your_target_line_here', 'your_code_here')  
+#inserter = CodeInserter(f'resources/cdsViews/core/I_HCM_ESSFamilyMemberTP', 'src/resources/cdsViews/sg/I_SG_HCM_ESSFamilyMemberTP')  
+input_filepath = os.path.join(os.getcwd(), 'src\\resources\\cdsViews\\core\\I_HCM_ESSFamilyMemberTP')  
+output_filepath = os.path.join(os.getcwd(), 'src\\resources\\cdsViews\\sg\\I_SG_HCM_ESSFamilyMemberTP')  
+inserter = CodeInserter(input_filepath, output_filepath)  
+inserter.insert_after('as select from I_HCM_ESSFamilyMember', 'cds_country_version')  
+
